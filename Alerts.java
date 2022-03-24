ad9c821bb8241b40edef8e21200b07340840d24a
Kumar <chamkumar@deloitte.com>
2022-03-24 11:08:44 +0530

    Scenario-3

---
+++ Alerts.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+public class Alerts{
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Chamkumar\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//a[@href='/javascript_alerts']")).click();
+        driver.findElement(By.xpath("//button[@onclick='jsPrompt()']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        String expected = "You entered: Ex-Test";
+        driver.switchTo().alert().sendKeys("Ex-Test");
+        Thread.sleep(2000);
+        driver.switchTo().alert().accept();
+        String result =driver.findElement(By.xpath("//p[@id='result']")).getText();
+        if (expected.equals(result) == true)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+}
+
